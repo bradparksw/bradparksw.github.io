7d3b29e98641ab7b18fd63193946615713ccc980
Brad <sw27park@edu.uwaterloo.ca>
2020-09-04 02:54:36 -0400

    added navbar toggle with animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,28 +7,28 @@ class Header extends Component {
       <nav class="navbar navbar-expand-md fixed-top" style={{backgroundColor:'#222629', padding: '1rem 2rem 1rem 2rem'}}>
         <a class="navbar-brand" href="/#">Fixed navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon">
+            <i class="fa fa-navicon" style={{color:'#fff', fontSize:'28px'}}></i>
+          </span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-          <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-              <ol class="navbar-nav ml-auto">
-                  <li class="nav-item">
-                      <a class="nav-link" href="/#about">About Me</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/#experience">Experience</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/#skills">Skills</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/#projects">Projects</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="/#contact">Contact Me</a>
-                  </li>
-              </ol>
-          </div>
+          <ol class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link" href="/#about">About Me</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="/#experience">Experience</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="/#skills">Skills</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="/#projects">Projects</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="/#contact">Contact Me</a>
+              </li>
+          </ol>
         </div>
       </nav>
     )
